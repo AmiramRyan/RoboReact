7f86a9e2c7e6ebf525ff9ebc610ec48d0f0fe4a0
AmiramRyan <Ryan4works@gmail.com>
2021-11-04 13:25:02 +0200

    More colors and synchronized movement
    -added Yellow and Green blocks
    -player can not rotate 2 colors at the same time
    -finished the scene lvl

--- Assets/Scripts/PlayerInteraction.cs
+++ Assets/Scripts/PlayerInteraction.cs
@@ -5,14 +5,63 @@ using UnityEngine;
 public class PlayerInteraction : MonoBehaviour
 {
     public RotatingBlock[] purpleBlocks;
+    public RotatingBlock[] yellowBlocks;
+    public RotatingBlock[] greenBlocks;
+    private bool rotatePurpleActive = false;
+    private bool rotateYellowActive = false;
+    private bool rotateGreenActive = false;
     private void Update()
     {
-        if (Input.GetKeyDown("1"))
+        if (Input.GetKeyDown("1") && OthersInactive("purple"))
         {
-            for(int i = 0; i < purpleBlocks.Length; i++)
-            {
-                purpleBlocks[i].rotate = !purpleBlocks[i].rotate;
-            }
+            rotatePurpleActive = !rotatePurpleActive;
+            SetRotation(purpleBlocks, rotatePurpleActive);
+        }
+        if (Input.GetKeyDown("2") && OthersInactive("yellow"))
+        {
+            rotateYellowActive = !rotateYellowActive;
+            SetRotation(yellowBlocks, rotateYellowActive);
+        }
+        if (Input.GetKeyDown("3") && OthersInactive("green"))
+        {
+            rotateGreenActive = !rotateGreenActive;
+            SetRotation(greenBlocks, rotateGreenActive);
+        }
+
+    }
+
+    private void SetRotation(RotatingBlock[] blocksArr, bool rotate)
+    {
+        for (int i = 0; i < blocksArr.Length; i++)
+        {
+            blocksArr[i].rotate = rotate;
+        }
+    }
+
+    private bool OthersInactive(string color)
+    {
+        switch (color)
+        {
+            case "purple":
+                if(rotateYellowActive || rotateGreenActive)
+                {
+                    return false;
+                }
+                else { return true; }
+            case "yellow":
+                if (rotatePurpleActive || rotateGreenActive)
+                {
+                    return false;
+                }
+                else { return true; }
+            case "green":
+                if (rotateYellowActive || rotatePurpleActive)
+                {
+                    return false;
+                }
+                else { return true; }
+            default:
+                return true;
         }
     }
 }
